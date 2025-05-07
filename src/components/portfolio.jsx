3125a30a93dafcbc49b63c73103a53a31ca16ef2
Sabas32 <sktechug@gmail.com>
2025-05-08 01:21:24 +0300

    updated the projects section

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -10,6 +10,7 @@ import { useInView } from "react-intersection-observer";
 const Portfolio = ({ setIsLoading }) => {
   const [portfolioArray, setPortfolioArray] = useState([]);
   const [filteredportfolioArray, setFilteredPortfolioArray] = useState([]);
+  const [activeFilter, setActiveFilter] = useState("All");
 
   useEffect(() => {
     setIsLoading(true);
@@ -37,6 +38,7 @@ const Portfolio = ({ setIsLoading }) => {
     filtered = portfolioArray;
 
     setFilteredPortfolioArray(filtered);
+    setActiveFilter("All");
     console.log(filtered);
   }
   function handleFilterToEach(searchQuery) {
@@ -46,6 +48,7 @@ const Portfolio = ({ setIsLoading }) => {
 
     filtered = portfolioArray.filter((e) => e.type === searchQuery);
     setFilteredPortfolioArray(filtered);
+    setActiveFilter(searchQuery);
     console.log(filtered);
   }
 
@@ -92,26 +95,36 @@ const Portfolio = ({ setIsLoading }) => {
         >
           <ul>
             <li>
-              <p className="nav-link" onClick={handleFilterToAll}>
+              <p
+                className={`nav-link ${activeFilter === "All" ? "active" : ""}`}
+                onClick={handleFilterToAll}
+              >
                 All
               </p>
             </li>
             <li>
               <p
-                className="nav-link"
+                className={`nav-link ${
+                  activeFilter === "Mobile App" ? "active" : ""
+                }`}
                 onClick={() => handleFilterToEach("Mobile App")}
               >
                 Mobile
               </p>
             </li>
             <li>
-              <p className="nav-link" onClick={() => handleFilterToEach("Web")}>
+              <p
+                className={`nav-link ${activeFilter === "Web" ? "active" : ""}`}
+                onClick={() => handleFilterToEach("Web")}
+              >
                 Web
               </p>
             </li>
             <li>
               <p
-                className="nav-link"
+                className={`nav-link ${
+                  activeFilter === "Desktop" ? "active" : ""
+                }`}
                 onClick={() => handleFilterToEach("Desktop")}
               >
                 Desktop
@@ -121,27 +134,36 @@ const Portfolio = ({ setIsLoading }) => {
         </div>
 
         <div className="projectsContainer">
+          {filteredportfolioArray.length === 0 && (
+            <div>
+              <h1 className="title-2">No Project Found</h1>
+            </div>
+          )}
           {filteredportfolioArray.map((e) => (
             <div>
               <div
                 // className="singleProject"
                 key={e.id}
-                className={`singleProject `}
+                className={`singleProject box`}
               >
-                <img
-                  src={`${e.project_background_image}`}
-                  alt="app-img"
-                  className="back-Img"
-                />
-                <div className="veiwProjectOverly-black"></div>
+                <div className="image">
+                  <img
+                    src={`${e.project_background_image}`}
+                    alt="app-img"
+                    className="back-Img"
+                  />
+                </div>
+                {/* <div className="veiwProjectOverly-black"></div> */}
                 <div className="veiwProjectOverly">
-                  <h3>{e.title}</h3>
-                  <h6>{e.type} Application</h6>
-                  <div className="madeWith">
-                    {e.tech_stack.map((stack, index) => (
-                      <p key={index}>#{stack.language}</p>
-                    ))}
-                    {/* <p>#django </p> */}
+                  <div className="info">
+                    <h3>{e.title}</h3>
+                    <h6>{e.type} Application</h6>
+                    <div className="madeWith">
+                      {e.tech_stack.map((stack, index) => (
+                        <p key={index}>#{stack.language}</p>
+                      ))}
+                      {/* <p>#django </p> */}
+                    </div>
                   </div>
                   <Link
                     to={`/portfolio/project/${e.id}/${e.type}`}
